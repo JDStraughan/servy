98efb8f5a7aad23736e061ea77b8d24250e3a2ec
JDStraughan <jdstraughan@gmail.com>
2022-08-01 16:48:14 -0500

    Moving conv map to struct

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,6 +4,8 @@ defmodule Servy.Handler do
 
   @pages_path Path.expand("../../pages", __DIR__)
 
+  alias Servy.Conv
+
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
 
@@ -18,32 +20,32 @@ defmodule Servy.Handler do
     |> format_response()
   end
 
-  def route(%{method: "GET", path: "/about" } = conv) do
+  def route(%Conv{method: "GET", path: "/about" } = conv) do
     @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "All the things"}
   end
 
-  def route(%{method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Da Bears"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bears ##{id}"}
   end
 
-  def route(%{ path: path} = conv) do
+  def route(%Conv{ path: path} = conv) do
     %{conv | status: 404, resp_body: "404: No #{path} found"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -61,17 +63,6 @@ defmodule Servy.Handler do
 
   defp handle_file({:error, reason}, conv) do
     %{ conv | status: 500, resp_body: "File error: #{reason}"}
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 
 end
