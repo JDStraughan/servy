66665c939ddd9146f36e5f96bcd88a815a8cc279
JDStraughan <jdstraughan@gmail.com>
2022-08-01 13:49:22 -0500

    Added pattern matching to routes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -15,29 +15,51 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+       path: path,
+       resp_body: "",
+       status: nil }
   end
 
   def log(conv), do: IO.inspect conv
 
-  def route(conv), do: route(conv, conv.path)
+  def route(conv), do: route(conv, conv.method, conv.path)
 
-  def route(conv, "/wildthings") do
-    %{ conv | resp_body: "All the things"}
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | status: 200, resp_body: "All the things"}
   end
 
-  def route(conv, "/bears") do
-    %{ conv | resp_body: "Da Bears"}
+  def route(conv, "GET", "/bears") do
+    %{ conv | status: 200, resp_body: "Da Bears"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bears ##{id}"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "404: No #{path} found"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 
 end
@@ -56,6 +78,30 @@ IO.puts response
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
