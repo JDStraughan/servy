4e6bd16eef3314c74fd01b6278ff6f5bfcc9f91a
JDStraughan <jdstraughan@gmail.com>
2022-08-01 19:23:57 -0500

    Using enum & controller structure for app

---
+++ lib/servy/bear.ex
@@ -0,0 +1,7 @@
+defmodule Servy.Bear do
+  defstruct id: nil, name: "", type: "", hibernating: false
+
+  def order_asc_by_name(a, b) do
+    a.name <= b.name
+  end
+end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,6 +5,7 @@ defmodule Servy.Handler do
   @pages_path Path.expand("../../pages", __DIR__)
 
   alias Servy.Conv
+  alias Servy.BearController
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
@@ -21,7 +22,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "POST", path: "/bears" } = conv) do
-    %{ conv | status: 201, resp_body: "New #{conv.params["type"]} bear named #{conv.params["name"]}!"}
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{method: "GET", path: "/about" } = conv) do
@@ -36,11 +37,12 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears" } = conv) do
-    %{ conv | status: 200, resp_body: "Da Bears"}
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "GET", path: "/bears/" <> id } = conv) do
-    %{ conv | status: 200, resp_body: "Bears ##{id}"}
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{ path: path} = conv) do
