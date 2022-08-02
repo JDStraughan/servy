9aed46d51678942a897b73488d7f62efcc7e1040
JDStraughan <jdstraughan@gmail.com>
2022-08-01 21:37:17 -0500

    More doc tests to make sure I understand them

--- lib/servy/bear.ex
+++ lib/servy/bear.ex
@@ -1,6 +1,15 @@
 defmodule Servy.Bear do
   defstruct id: nil, name: "", type: "", hibernating: false
 
+  @doc """
+  Checks if a.name is before b.name
+
+  ## Example
+    iex> Servy.Bear.order_asc_by_name(%{name: "Zelda"}, %{name: "Link"})
+    false
+    iex> Servy.Bear.order_asc_by_name(%{name: "Link"}, %{name: "Zelda"})
+    true
+  """
   def order_asc_by_name(a, b) do
     a.name <= b.name
   end
